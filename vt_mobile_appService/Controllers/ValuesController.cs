8d64f97f23cad45f8b81e564c6d9d6c24065bead
Cezary <cezary.katner@vontobel.ch>
2016-06-06 15:50:27 +0200

    test

--- vt_mobile_appService/Controllers/ValuesController.cs
+++ vt_mobile_appService/Controllers/ValuesController.cs
@@ -17,7 +17,7 @@ namespace vt_mobile_appService.Controllers
             ITraceWriter traceWriter = this.Configuration.Services.GetTraceWriter();
 
             string host = settings.HostName ?? "localhost";
-            string greeting = "Hello from " + host;
+            string greeting = "Hello1 from " + host;
             
             traceWriter.Info(greeting);
             return greeting;
